f5a0547748cd8f6226416cef2f816f3cc73451ec
Kushagra Chourasia <kush@Kushagras-Laptop.local>
2026-02-20 01:41:35 +0000

    Added operations on lists and dictionaries

---
+++ Lists&Dictionaries.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 20 01:31:31 2026
+
+@author: kush
+"""
+
+import numpy as np
+import pandas as pd
+import statistics
+#%% Operations on list
+numbers = [10, 20, 30, 40, 50]
+nums = numbers.copy()
+nums
